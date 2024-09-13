d22b1689ae1a9e77bc7419d5396f45fed6a4248b
vahidSpectre <vahidsayfollahzadeh@gmail.com>
2024-09-13 18:43:07 +0330

    1

---
+++ server/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(
+  cors({
+    origin: '*',
+    credentials: true,
+    allowedHeaders: ['Authorization', 'Content-Type'],
+  }),
+);
+
+app.get('/', (req, res, next) => {
+  res.status(200).json({
+    message: 'server is up and running successfully!',
+  });
+  next();
+});
+
+app.listen(8080, () => {
+  console.log('server is running');
+});
